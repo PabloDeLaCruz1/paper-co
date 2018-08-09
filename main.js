a4fc648c81cd776f1a6faadee716efa7e1d44bd6
Pablo De La Cruz <squall07524@gmail.com>
2018-08-08 21:02:43 -0400

    css features

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-document.getElementById('theme-button').onclick = function () {
-    let theme = document.getElementById('theme-css');
-    console.log(theme, "hiii");
-    
-    if (theme === "main.css") {
-        theme = "main-alt.css";
-    } else {
-        theme = "main.css";
-    }
+document.getElementById('theme-button').onclick = function () { 
+
+document.getElementById('theme-css').href = "main-alt.css";
 };
